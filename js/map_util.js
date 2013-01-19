ddcfcd4d5d1eb52faeecce5a202ac2efdcb16113
FurtherLee <95588lsj@gmail.com>
2013-01-20 00:55:01 +0800

    add more stubs, leave occupiedByPlane undone

--- js/map_util.js
+++ js/map_util.js
@@ -1,7 +1,16 @@
 // Define direction
-var UP = 0, RIGHT = 1, DOWN = 2, LEFT = 3;
+var UP = 0, RIGHT = 1, DOWN = 2, LEFT = 3, LAST_D = 4;
 var DIM = 10;
 var NuM_PLANE = 3;
+
+/**
+ * This function will tell you which units this plane will ocupy
+ * params: a plane
+ * return: a list of (x, y), where the plane will occupy
+ */
+var occupiedByPlane(plane){
+    // TODO
+}
 
 var generate_map = function() {
     
@@ -16,15 +25,42 @@ var generate_map = function() {
     }
     
     function getRandomPlane() {
-	// TODO
+
+	function outOfBound(plane){
+	    for (pair in occupiedByPlane(plane))
+		if (pair['x'] < 0 || pair['x'] >= DIM ||
+		    pair['y'] < 0 || pair['y'] >= DIM)
+		    return true;
+	    return false;
+	}
+	
+	function getRandomNum(min, max){
+	    var range = max - min;
+	    var rand = Math.random();
+	    return Min + Math.floor(rand*range);
+	}
+
+	var plane;
+	do{
+	    plane = {
+		x: getRandomNum(0, DIM),
+		y: getRandomNum(0, DIM),
+		d: getRandomNum(0, LAST_D);
+	    };
+	} while(outOfBound(plane));
+	return plane;
     }
 
     function compatible(plane, bitmap) {
-	// TODO
+	for (pair in occupiedByPlane(plane))
+	    if (bitmap[pair['x']][pair['y']] == 1)
+		return false;
+	return true;
     }
 
     function drawPlaneToBitmap(plane, bitmap) {
-	// TODO
+	for (pair in occupiedByPlane(plane))
+	     bitmap[pair['x']][pair['y']] = 1;
     }
 
     var bitMap = genZeroMap();
