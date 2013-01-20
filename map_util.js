9dd3a14e80ce861732d68baea5b1feffd3f8dd0d
FurtherLee <95588lsj@gmail.com>
2013-01-20 11:11:58 +0800

    envolop map_util into jquery space

--- map_util.js
+++ map_util.js
@@ -1,147 +1,149 @@
-// Define direction
-var UP = 0, RIGHT = 1, DOWN = 2, LEFT = 3, LAST_D = 4;
-var DIM = 10;
-var NUM_PLANE = 3;
+(function($) {
+    // Define direction
+    var UP = 0, RIGHT = 1, DOWN = 2, LEFT = 3, LAST_D = 4;
+    var DIM = 10;
+    var NUM_PLANE = 3;
 
-/**
- * This function will tell you which units this plane will ocupy
- * params: a plane
- * return: a list of (x, y), where the plane will occupy
- */
-function occupiedByPlane(plane) {
+    /**
+     * This function will tell you which units this plane will ocupy
+     * params: a plane
+     * return: a list of (x, y), where the plane will occupy
+     */
+    function occupiedByPlane(plane) {
 
-    function rotate (fixpoint, points) {
-        var ret = new Array();
-        for (var i = 0; i < points.length; ++i){
-            var point = points[i];
-            ret.push({x: fixpoint['x'] + (point['y'] - fixpoint['y']), 
-                      y: fixpoint['y'] - (point['x'] - fixpoint['x'])});
-        }
-        return ret;
-    }
-
-    function pointsOfUpPlane(plane) {
-
-        var ret = new Array();
-        ret.push({x: plane['x'], y:plane['y']});
-
-        for (var i = -2; i<= 2; ++i)
-            ret.push({x: plane['x']+i, y:plane['y']+1});
-
-        ret.push({x: plane['x']+2, y:plane['y']+2});
-
-        for (var i = -1; i<= 1; ++i)
-            ret.push({x: plane['x']+i, y:plane['y']+3});
-
-        return ret;
-    }
-    
-    var ret = pointsOfUpPlane(plane);
-    var head = {
-        x: plane['x'],
-        y: plane['y']
-    };
-    
-    for (var i = 0; i < plane['d']; ++i)
-        ret = rotate(head, ret);
-
-    return ret;
-}
-
-var generate_map = function() {
-    
-    function genZeroMap() {
-        var ret = new Array();
-        for (var i = 0; i < DIM; ++i){
-            ret[i] = new Array();
-            for (var j = 0; j < DIM; ++j)
-                ret[i][j] = 0;
-        }
-        return ret;
-    }
-    
-    function getRandomPlane() {
-
-        function outOfBound(plane){
-            var points = occupiedByPlane(plane);
-            for (var i = 0; i < points.length; ++i) {
-                var pair = points[i];
-                if (pair['x'] < 0 || pair['x'] >= DIM ||
-                    pair['y'] < 0 || pair['y'] >= DIM)
-                    return true;
+	function rotate (fixpoint, points) {
+            var ret = new Array();
+            for (var i = 0; i < points.length; ++i){
+		var point = points[i];
+		ret.push({x: fixpoint['x'] + (point['y'] - fixpoint['y']), 
+			  y: fixpoint['y'] - (point['x'] - fixpoint['x'])});
             }
-            return false;
-        }
+            return ret;
+	}
 
-        function getRandomNum(min, max){
-            var range = max - min;
-            var rand = Math.random();
-            return min + Math.floor(rand*range);
-        }
+	function pointsOfUpPlane(plane) {
 
-        var plane;
-        do{
-            plane = {
-                x: getRandomNum(0, DIM),
-                y: getRandomNum(0, DIM),
-                d: getRandomNum(0, LAST_D)
-            };
-        } while(outOfBound(plane));
-        return plane;
+            var ret = new Array();
+            ret.push({x: plane['x'], y:plane['y']});
+
+            for (var i = -2; i<= 2; ++i)
+		ret.push({x: plane['x']+i, y:plane['y']+1});
+
+            ret.push({x: plane['x']+2, y:plane['y']+2});
+
+            for (var i = -1; i<= 1; ++i)
+		ret.push({x: plane['x']+i, y:plane['y']+3});
+
+            return ret;
+	}
+	
+	var ret = pointsOfUpPlane(plane);
+	var head = {
+            x: plane['x'],
+            y: plane['y']
+	};
+	
+	for (var i = 0; i < plane['d']; ++i)
+            ret = rotate(head, ret);
+
+	return ret;
     }
 
-    function compatible(plane, bitmap) {
-        var points = occupiedByPlane(plane);
-        for (var i = 0; i < points.length; ++i){
-            var pair = points[i];
-            if (bitmap[pair['x']][pair['y']] == 1)
-                return false;
-        }
-        return true;
-    }
+    function generate_map() {
+	
+	function genZeroMap() {
+            var ret = new Array();
+            for (var i = 0; i < DIM; ++i){
+		ret[i] = new Array();
+		for (var j = 0; j < DIM; ++j)
+                    ret[i][j] = 0;
+            }
+            return ret;
+	}
+	
+	function getRandomPlane() {
 
-    function drawPlaneToBitmap(plane, bitmap) {
-        var points = occupiedByPlane(plane);
-        for (var i = 0; i < points.length; ++i){
-            var pair = points[i];
-            bitmap[pair['x']][pair['y']] = 1;
-        }
-    }
+            function outOfBound(plane){
+		var points = occupiedByPlane(plane);
+		for (var i = 0; i < points.length; ++i) {
+                    var pair = points[i];
+                    if (pair['x'] < 0 || pair['x'] >= DIM ||
+			pair['y'] < 0 || pair['y'] >= DIM)
+			return true;
+		}
+		return false;
+            }
 
-    var bitMap = genZeroMap();
-    
-    var ans = new Array();
+            function getRandomNum(min, max){
+		var range = max - min;
+		var rand = Math.random();
+		return min + Math.floor(rand*range);
+            }
 
-    for (var i = 0; i < NUM_PLANE; ++i){
-        var plane;
+            var plane;
+            do{
+		plane = {
+                    x: getRandomNum(0, DIM),
+                    y: getRandomNum(0, DIM),
+                    d: getRandomNum(0, LAST_D)
+		};
+            } while(outOfBound(plane));
+            return plane;
+	}
 
-        do{
-            plane = getRandomPlane();
-        } while(!compatible(plane, bitMap));
+	function compatible(plane, bitmap) {
+            var points = occupiedByPlane(plane);
+            for (var i = 0; i < points.length; ++i){
+		var pair = points[i];
+		if (bitmap[pair['x']][pair['y']] == 1)
+                    return false;
+            }
+            return true;
+	}
 
-        drawPlaneToBitmap(plane, bitMap);
+	function drawPlaneToBitmap(plane, bitmap) {
+            var points = occupiedByPlane(plane);
+            for (var i = 0; i < points.length; ++i){
+		var pair = points[i];
+		bitmap[pair['x']][pair['y']] = 1;
+            }
+	}
 
-        ans[i] = plane;
-    }
+	var bitMap = genZeroMap();
+	
+	var ans = new Array();
 
-    // Add the fucking offset
-    for (var point in ans){
-        switch(point['d']){
-        case UP:
-            point['x'] -= 2;
-            break;
-        case RIGHT:
-            point['x'] -= 3;
-            point['y'] -= 2;
-            break;
-        case DOWN:
-            point['x'] -= 2;
-            point['y'] -= 3;
-            break;
-        case LEFT:
-            point['y'] -= 2;
-            break; 
-        }
-    }
-    return ans;
+	for (var i = 0; i < NUM_PLANE; ++i){
+            var plane;
+
+            do{
+		plane = getRandomPlane();
+            } while(!compatible(plane, bitMap));
+
+            drawPlaneToBitmap(plane, bitMap);
+
+            ans[i] = plane;
+	}
+
+	// Add the fucking offset
+	for (var point in ans){
+            switch(point['d']){
+            case UP:
+		point['x'] -= 2;
+		break;
+            case RIGHT:
+		point['x'] -= 3;
+		point['y'] -= 2;
+		break;
+            case DOWN:
+		point['x'] -= 2;
+		point['y'] -= 3;
+		break;
+            case LEFT:
+		point['y'] -= 2;
+		break; 
+            }
+	}
+	return ans;
+    };
 };
